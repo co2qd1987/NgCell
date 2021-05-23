0a01cfd60419d168f9f4abd9c7f2bafe7075d583
co2qd1987 <co2qd@sina.com>
2021-05-23 11:58:06 +0800

    20210522

--- libapng/MainRender.java
+++ libapng/MainRender.java
@@ -13,10 +13,16 @@ public final class MainRender extends ApngRenderImpl {
     public void play() {
         ID id = new ID();
         try{
+
             List<BufferedImage> frames = decoder();
             for(BufferedImage image : frames) {
                 ImageIO.write(image,"PNG",new File(Main.class.getResource("").getPath() + "IMG" + id.getID() + ".png"));
             }
+            System.out.println("-----" + getName() + "-----");
+            System.out.println("Hight: " + getHight() + ",Width: " + getWidth());
+            System.out.println(getAnimateControl().toString());
+            System.out.println(getFrameControl().get(0).toString() + " isSkipFirstFrame: " + isSkipFirstFrame());
+            System.out.println("-----End-----");
         } catch (Exception e) {
             log.log(Level.WARNING, "could not play", e);
         }
